545f4ed049a951ee230770bc2262f787c3b554b3
Eric Liu <eric.liu@modernfi.com>
2025-03-24 21:58:33 -0700

    add create google calendar meeting action

---
+++ src/actions/providers/google-oauth/scheduleCalendarMeeting.ts
@@ -0,0 +1,75 @@
+import {
+  AuthParamsType,
+  googleOauthScheduleCalendarMeetingFunction,
+  googleOauthScheduleCalendarMeetingOutputType,
+  googleOauthScheduleCalendarMeetingParamsType,
+} from "../../autogen/types";
+import { axiosClient } from "../../util/axiosClient";
+
+/**
+ * Creates a new Google calendar event using OAuth authentication
+ */
+const scheduleCalendarMeeting: googleOauthScheduleCalendarMeetingFunction = async ({
+  params,
+  authParams,
+}: {
+  params: googleOauthScheduleCalendarMeetingParamsType;
+  authParams: AuthParamsType;
+}): Promise<googleOauthScheduleCalendarMeetingOutputType> => {
+  if (!authParams.authToken) {
+    throw new Error("authToken is required for Google Docs API");
+  }
+  const { calendarId, name, start, end, description, attendees } = params;
+  // https://developers.google.com/calendar/api/v3/reference/events/insert
+  const createEventApiUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`;
+
+  const data: {
+    summary: string;
+    start: {
+      dateTime: string;
+    };
+    end: {
+      dateTime: string;
+    };
+    description?: string;
+    attendees?: string[];
+  } = {
+    summary: name,
+    start: {
+      dateTime: start,
+    },
+    end: {
+      dateTime: end,
+    },
+  };
+
+  if (description) {
+    data.description = description;
+  }
+
+  if (attendees) {
+    data.attendees = attendees;
+  }
+
+  const response = await axiosClient.post(createEventApiUrl, data, {
+    headers: {
+      Authorization: `Bearer ${authParams.authToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.status !== 200) {
+    return {
+      success: false,
+      error: response.data.error,
+    };
+  }
+
+  return {
+    success: true,
+    eventId: response.data.id,
+    eventUrl: response.data.htmlLink,
+  };
+};
+
+export default scheduleCalendarMeeting;
---
+++ tests/testGoogleScheduleMeeting.ts
@@ -0,0 +1,48 @@
+import {
+  googleOauthScheduleCalendarMeetingOutputType,
+  googleOauthScheduleCalendarMeetingParamsType,
+} from "../src/actions/autogen/types";
+import { runAction } from "../src/app";
+import assert from "node:assert";
+
+/**
+ * Test for the Google OAuth createNewGoogleDoc action
+ */
+async function runTest() {
+  console.log("Running test for Google OAuth createNewGoogleDoc");
+
+  const authToken = "insert-access-token"; // Test with token from: https://developers.google.com/oauthplayground/
+  const calendarId = "insert-calendar-id";
+  const result: googleOauthScheduleCalendarMeetingOutputType = await runAction(
+    "googleCalendarScheduleMeeting",
+    "googleOauth",
+    {
+      authToken,
+    },
+    {
+      calendarId,
+      name: "Credal Test Meeting",
+      start: new Date().toISOString(),
+      end: new Date(new Date().getTime() + 1000 * 60 * 60).toISOString(),
+      description:
+        "This is a test meeting created automatically by the actions-sdk test suite.",
+      attendees: ["test@test.com", "test2@test.com"],
+    } as googleOauthScheduleCalendarMeetingParamsType
+  );
+
+  console.log("Result:", result);
+
+  // Validate the result
+  assert(result.eventId, "Result should be successful");
+  assert(result.success, "Result should contain an eventId");
+
+  console.log("Link to Google Calendar Event: ", result.eventUrl);
+
+  return result;
+}
+
+// Run the test
+runTest().catch((error) => {
+  console.error("Test execution failed:", error);
+  process.exit(1);
+});
