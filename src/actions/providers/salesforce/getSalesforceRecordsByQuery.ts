db8f569a8f30e1acf4aae44860d3bba6fdcc2075
Livia Seibert <livia@credal.ai>
2025-07-02 11:17:08 -0400

    fix salesforce limit

--- src/actions/providers/salesforce/getSalesforceRecordsByQuery.ts
+++ src/actions/providers/salesforce/getSalesforceRecordsByQuery.ts
@@ -22,9 +22,15 @@ const getSalesforceRecordsByQuery: salesforceGetSalesforceRecordsByQueryFunction
       error: "authToken and baseUrl are required for Salesforce API",
     };
   }
+  // Included a prepended space and an opening bracket to make sure these terms don't get confused
+  // with parts of other words.
+  const aggregateFunction = [" COUNT(", " SUM(", " AVG(", " MIN(", " MAX("];
+  const containsAggregateFunction = aggregateFunction.some(func => query.toUpperCase().includes(func));
 
   // The API limits the maximum number of records returned to 2000, the limit lets the user set a smaller custom limit
-  const url = `${baseUrl}/services/data/v56.0/queryAll?q=${encodeURIComponent(query + " LIMIT " + (limit != undefined && limit <= 2000 ? limit : 2000))}`;
+  const url = `${baseUrl}/services/data/v56.0/queryAll?q=${encodeURIComponent(
+    containsAggregateFunction ? query : query + " LIMIT " + (limit != undefined && limit <= 2000 ? limit : 2000)
+  )}`;
 
   try {
     const response = await axiosClient.get(url, {
