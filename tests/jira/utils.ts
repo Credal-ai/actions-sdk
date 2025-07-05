303be2bc53c579e7b2c8cbf1a70653521093f2a6
Ria Balli <ria@credal.ai>
2025-07-05 12:39:34 -0400

    revert plac eholders

--- tests/jira/utils.ts
+++ tests/jira/utils.ts
@@ -1,11 +1,11 @@
 export const jiraConfig = {
   // For OAuth Credentials with Jira: https://developer.atlassian.com/cloud/jira/platform/oauth-2-3lo-apps/
-  authToken: "<your-auth-token-here>",
+  authToken: "your-auth-token-here",
   cloudId: "your-cloud-id-here",
-  baseUrl: "<your-base-url-here>",
-  projectKey: "your-project-key-here",
+  baseUrl: "your-base-url-here",
+  projectKey: "your-projectkey-here",
   issueId: "your-issueId-here",
-  assignee: "your-assignee-email-or-accountId-here",
+  assignee: "your-assignee-here",
 };
 
 export const provider = "jira";
