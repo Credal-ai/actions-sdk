bdc987cc5cfd95c69d1841d66a783c7ca36a2723
Livia Seibert <livia@credal.ai>
2025-07-09 12:12:02 -0400

    more

--- src/actions/actionMapper.ts
+++ src/actions/actionMapper.ts
@@ -230,6 +230,10 @@ import {
   githubSearchRepositoryParamsSchema,
   salesforceSearchSalesforceRecordsParamsSchema,
   salesforceSearchSalesforceRecordsOutputSchema,
+  googleOauthGetDriveFileContentByIdOutputSchema,
+  googleOauthGetDriveFileContentByIdParamsSchema,
+  googleOauthSearchDriveByQueryOutputSchema,
+  googleOauthSearchDriveByQueryParamsSchema,
 } from "./autogen/types.js";
 import validateAddress from "./providers/googlemaps/validateAddress.js";
 import add from "./providers/math/add.js";
@@ -345,6 +349,8 @@ import searchRepository from "./providers/github/searchRepository.js";
 import getServiceDesks from "./providers/jira/getServiceDesks.js";
 import createServiceDeskRequest from "./providers/jira/createServiceDeskRequest.js";
 import searchSalesforceRecords from "./providers/salesforce/searchSalesforceRecords.js";
+import getDriveFileContentById from "./providers/google-oauth/getDriveFileContentById.js";
+import searchDriveByQuery from "./providers/google-oauth/searchDriveByQuery.js";
 
 interface ActionFunctionComponents {
   // eslint-disable-next-line
