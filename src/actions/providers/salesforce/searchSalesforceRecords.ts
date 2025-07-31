ab32d29ccf6198ebd3ef760418a6063475bc56ed
Akul Kapoor <akulkapoor@Akuls-MacBook-Pro.local>
2025-07-31 14:49:00 -0400

    Clean up

--- src/actions/providers/salesforce/searchSalesforceRecords.ts
+++ src/actions/providers/salesforce/searchSalesforceRecords.ts
@@ -15,6 +15,7 @@ const searchSalesforceRecords: salesforceSearchSalesforceRecordsFunction = async
 }): Promise<salesforceSearchSalesforceRecordsOutputType> => {
   const { authToken, baseUrl } = authParams;
   const { keyword, recordType, fieldsToSearch } = params;
+  const searchFields = Array.from(new Set([...fieldsToSearch, "Id"]));
 
   if (!authToken || !baseUrl) {
     return {
@@ -23,9 +24,9 @@ const searchSalesforceRecords: salesforceSearchSalesforceRecordsFunction = async
     };
   }
   const maxLimit = 25;
-  const dateFieldExists = fieldsToSearch.includes("CreatedDate");
+  const dateFieldExists = searchFields.includes("CreatedDate");
   const url = `${baseUrl}/services/data/v64.0/search/?q=${encodeURIComponent(
-    `FIND {${keyword}} RETURNING ${recordType} (${fieldsToSearch.join(", ") + (dateFieldExists ? " ORDER BY CreatedDate DESC" : "")}) LIMIT ${params.limit && params.limit <= maxLimit ? params.limit : maxLimit}`,
+    `FIND {${keyword}} RETURNING ${recordType} (${searchFields.join(", ") + (dateFieldExists ? " ORDER BY CreatedDate DESC" : "")}) LIMIT ${params.limit && params.limit <= maxLimit ? params.limit : maxLimit}`,
   )}`;
 
   try {
