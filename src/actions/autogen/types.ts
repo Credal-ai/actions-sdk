3e574540a3626781f1afdb636d71c38729b29786
Jack Fischer <jack@credal.ai>
2025-08-20 14:32:26 -0400

    done

--- src/actions/autogen/types.ts
+++ src/actions/autogen/types.ts
@@ -58,7 +58,6 @@ export const AuthParamsSchema = z.object({
   tenantId: z.string().optional(),
   refreshToken: z.string().optional(),
   redirectUri: z.string().optional(),
-  userEmail: z.string().optional(),
 });
 
 export type AuthParamsType = z.infer<typeof AuthParamsSchema>;
