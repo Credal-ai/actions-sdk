3341f81beee8aa843d9985ae94362a5de78a664e
akulkapoor <akulkapoor@gmail.com>
2025-10-23 17:53:24 -0400

    Update search github action to return the standardized search action format (#412)
    * Update search github action to return the standardized search action format
    
    * build
    
    * fix
    
    * oneof - drop support
    
    ---------
    
    Co-authored-by: Akul Kapoor <akulkapoor@Akuls-MacBook-Pro.local>

--- src/actions/providers/github/searchOrganization.ts
+++ src/actions/providers/github/searchOrganization.ts
@@ -221,19 +221,19 @@ const searchOrganization: githubSearchOrganizationFunction = async ({
         type: "code" as const,
         name: result.name,
         url: result.url,
-        content: result,
+        contents: result,
       })),
       ...enrichedCommits.map(result => ({
         type: "commit" as const,
         name: result.sha,
         url: result.url,
-        content: result,
+        contents: result,
       })),
       ...issuesAndPRs.map(result => ({
         type: "issueOrPullRequest" as const,
         name: result.title,
         url: result.html_url,
-        content: result,
+        contents: result,
       })),
     ],
   };
