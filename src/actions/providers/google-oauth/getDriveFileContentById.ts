6765fb4377107b34210104039ed00a2ffc964b81
Jack Fischer <jack@credal.ai>
2026-09-12 15:25:02 -0400

    Upgrade vulnerable document parser dependencies

--- src/actions/providers/google-oauth/getDriveFileContentById.ts
+++ src/actions/providers/google-oauth/getDriveFileContentById.ts
@@ -154,7 +154,8 @@ const getDriveFileContentById: googleOauthGetDriveFileContentByIdFunction = asyn
       try {
         // officeparser expects a Buffer, so convert the ArrayBuffer
         const buffer = Buffer.from(downloadRes.data);
-        content = await officeParser.parseOfficeAsync(buffer);
+        const parsedOfficeFile = await officeParser.parseOffice(buffer);
+        content = parsedOfficeFile.toText();
       } catch (powerpointError) {
         return {
           success: false,
